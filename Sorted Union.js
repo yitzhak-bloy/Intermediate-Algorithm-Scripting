8ba6b98a53bf312878083461d2e5579c4e3a6825
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-07-28 11:13:08 +0300

    Sorted Union fix

--- Sorted Union.js
+++ Sorted Union.js
@@ -1,4 +1,18 @@
-const uniteUnique = (...arr) => [].concat(...arr).filter((num, index, arr) =>  arr.indexOf(num) == index)
+/*
+Intermediate Algorithm Scripting: Sorted Union
 
+Write a function that takes two or more arrays and returns a new array
+of unique values in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included in their original order, 
+but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, 
+but the final array should not be sorted in numerical order.
+
+Check the assertion tests for examples.
+*/
+
+const uniteUnique = (...arr) => [].concat(...arr).filter((num, index, arr) => arr.indexOf(num) === index);
 
 uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
