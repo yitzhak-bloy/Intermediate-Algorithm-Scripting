c425c9992337edc0f7f419155e797fccef64da7d
yitzhak-bloy <41252020+yitzhak-bloy@users.noreply.github.com>
2020-09-07 16:34:23 +0300

    Everything Be True

---
+++ Everything Be True.js
@@ -0,0 +1,25 @@
+/*
+Intermediate Algorithm Scripting: Everything Be True
+
+Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+In other words, you are given an array collection of objects. The predicate pre will be 
+an object property and you need to return true if its value is truthy. Otherwise, return false.
+
+In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+
+Remember, you can access object properties through either dot notation or [] notation.
+*/
+
+const truthCheck = (collection, pre) => {
+
+  for (const i in collection) {
+    if (!collection[i][pre]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex")
